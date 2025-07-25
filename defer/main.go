1e4529f5c07c87d5d4e43d322681136154e5b9db
oceanh6672@gmail.com <oceanh6672@gmail.com>
2025-07-26 05:54:25 +0600

    defer

--- defer/main.go
+++ defer/main.go
@@ -2,10 +2,17 @@ package defer1
 
 /*
 
-var a
-defer fmt.Println(a)  // in there, fmt is package & Println is function
+ - named return value
 
-defer function isn't executed immidiately, it is stored first. Go runtime runs the function (ectually everything is executed by cpu) just before executing return
+ - var a
+   defer fmt.Println(a)  // in there, fmt is package & Println is function
 
+ - defer function isn't executed immidiately, it is stored first. Go runtime runs the function (ectually everything is executed by cpu) just before executing return
 
+ - defer kutai store hoi ?
+    linked list
+
+ - defer kn data structure maintain kore ?
+   linked list data structure maintain kore.
+   stack er behaviour execute kore.
 */
