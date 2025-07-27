b7a04694a9a9bc52b52e95f0a74e564dd3dcd02f
oceanh6672@gmail.com <oceanh6672@gmail.com>
2025-07-27 08:43:31 +0600

    cp

--- prob_solving/A. AntonAndDanik/main.go
+++ prob_solving/A. AntonAndDanik/main.go
@@ -8,11 +8,46 @@ package main
 import "fmt"
 
 func main() {
+	var n int
 	var str string
-	fmt.Scan(&str)
+	fmt.Scan(&n, &str)
 
 	var a, d int
-	for i := 0; i < len(str); i++{
+	for i := 0; i < n; i++ {
+		if str[i] == 'A' {
+			a++
+		} else if str[i] == 'D' {
+			d++
+		}
+	}
+
+	if a > d {
+		fmt.Println("Anton")
+	} else if d > a {
+		fmt.Println("Danik")
+	} else {
+		fmt.Println("Friendship")
+	}
+}
+
+/* Alternative (More right code but lengthy)
+
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	fmt.Scanf("\n")    // consume the leftover newline
+
+	str := make([]byte, n)
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%c", &str[i]) // no space needed now, we handled newline
+	}
+
+	var a, d int
+	for i := 0; i < n; i++{
 		if str[i] == 'A'{
 			a++
 		} else if str[i] == 'D'{
@@ -22,29 +57,11 @@ func main() {
 
 	if a > d{
 		fmt.Println("Anton")
-	} else if d < a{
+	} else if d > a{
 		fmt.Println("Danik")
 	} else {
 		fmt.Println("Friendship")
 	}
-}
+} */
 
-
-/* 
-package main
-
-import "fmt"
-
-func main() {
-	var n int
-	fmt.Scan(&n)
-
-	str := make([]byte, n)
-
-	for i := 0; i < n; i++ {
-		fmt.Scanf(" %c", &str[i]) // space before %c to skip newline/space
-	}
-
-	fmt.Println(string(str))
-}
- */
+// For print the string : 	fmt.Println(string(str))
