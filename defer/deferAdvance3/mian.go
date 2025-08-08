e3221ac9368a295f35cd43cf49d703c20903b46d
oceanh6672@gmail.com <oceanh6672@gmail.com>
2025-08-08 18:28:46 +0600

    commited

--- defer/deferAdvance3/mian.go
+++ defer/deferAdvance3/mian.go
@@ -1,3 +1,54 @@
+// package main
+
+// import "fmt"
+
+// func calculate() (result int) {
+// 	fmt.Println("first", result)
+
+// 	show := func(result int) {
+// 		result = result + 10
+// 		fmt.Println("show", result)
+// 	}
+
+// 	defer show(result)
+
+// 	result = 5
+
+// 	p := func(a int) {
+// 		fmt.Println("p", a)
+// 	}
+
+// 	defer p(result)
+
+// 	defer fmt.Println("print1", result)
+
+// 	result = 10
+
+// 	fmt.Println("second", result)
+
+// 	defer fmt.Println("print2(direct 5)", 5)
+
+// 	return
+// }
+
+// func main() {
+// 	punch := calculate()
+// 	fmt.Println("return", punch)
+// }
+
+/*
+
+first 0
+defer show(0)
+defer p(5)
+defer Println(5)
+second 10
+defer Println(direct 5)
+
+return result
+
+*/
+
 package main
 
 import "fmt"
@@ -14,11 +65,11 @@ func calculate() (result int) {
 
 	result = 5
 
-	p := func(a int) {
-		fmt.Println("p", a)
+	p := func() {
+		fmt.Println("p", result)
 	}
 
-	defer p(result)
+	defer p()
 
 	defer fmt.Println("print1", result)
 
@@ -32,21 +83,19 @@ func calculate() (result int) {
 }
 
 func main() {
-	punch := calculate() 
+	punch := calculate()
 	fmt.Println("return", punch)
 }
 
+/*
 
+	first 0
+	defer show(0)
+	defer p()
+	defer println(5)
+	second(10)
+	defer println(direct 5)
 
-/* 
-
-first 0
-defer show[ref - result = 0, arg pass = 0]
-defer p[ref - result = 5, arg pass = 5]
-defer Println[ref - result]
-second 10
-defer Println[direct 5]
-
-return
+	return result
 
 */
